30bfbec8a96aef32b16b52fe7c1b50dc1738e779
joaquimmateus0404 <joaquimmateus0404@gmail.com>
2025-07-03 21:08:31 +0100

    feat: implementar exibição de data e hora na calculadora

--- scripts/controllers/CalcController.js
+++ scripts/controllers/CalcController.js
@@ -1,21 +1,67 @@
 class CalcController {
     constructor() {
-        this.displayCalc = "0";
-        this.correntDate = new Date();
+        this._locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._correntDate;
+        this.initialize();
     }
 
+    initialize() {
+
+        this.setDisplayDateTime();
+
+        setInterval(() => {
+
+           this.setDisplayDateTime();
+           
+        }, 1000);
+    }
+
+    setDisplayDateTime() {
+        this.displayTime = this.correntDate.toLocaleTimeString(this._locale, {
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit"
+        });
+        this.displayDate = this.correntDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric"
+        });
+    }
+
+
+    get displayTime() {
+        return this._timeEl.innerHTML;
+    }
+    set displayTime(value) {
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayDate() {
+        return this._dateEl.innerHTML;
+    }
+    set displayDate(value) {
+        this._dateEl.innerHTML = value;
+    }
+
+
     get displayCalc() {
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
-    set displayCalc(valor) {
-        this._displayCalc = valor;
+    set displayCalc(value) {
+        this._displayCalcEl.innerHTML = value;
     }
+
+
     get correntDate() {
-        return this._correntDate;
+        return new Date();
     }
-    set correntDate(valor) {
-        if (valor instanceof Date) {
-            this._correntDate = valor;
+    set correntDate(value) {
+        if (value instanceof Date) {
+            this._correntDate = value;
         } else {
             throw new Error("Data inválida");
         }
