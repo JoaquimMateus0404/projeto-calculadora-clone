e22bef6b1f6b7e8db9b9dc2efa5cec1313b9a12f
JoaquimMateus0404 <joaquimmateus0404@gmail.com>
2025-07-21 16:35:17 +0100

    feat: adicionar lógica de operações e manipulação de eventos na classe CalcController

--- scripts/controllers/CalcController.js
+++ scripts/controllers/CalcController.js
@@ -22,6 +22,8 @@ class CalcController {
         }, 1000);
     }
 
+    // Inicializa os eventos dos botões
+    // e adiciona ouvintes de eventos para os botões
     initButtonEvents() {
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
@@ -36,6 +38,9 @@ class CalcController {
             });
         });
     }
+
+    // Executa a ação do botão pressionado
+    // dependendo do valor do botão
     execBtn(value) {
         switch (value) {
             case 'ac':
@@ -45,19 +50,25 @@ class CalcController {
                 this.clearEntry();
                 break;
             case 'soma':
-
+                this.addOperation('+');
                 break;
             case 'subtracao':
+                this.addOperation('-');
                 break;
             case 'multiplicacao':
+                this.addOperation('*');
                 break;
             case 'divisao':
+                this.addOperation('/');
                 break; 
             case 'porcento':
+                this.addOperation('%');
                 break;
             case 'ponto':
+                this.addOperation('.');
                 break;
             case 'igual':
+                this.addOperation('');
                 break;
             case '0':
             case '1':
@@ -71,7 +82,7 @@ class CalcController {
             case '9':
                 this.addOperation(parseInt(value));
                 console.log(this._operation);
-                this.displayCalc = this._operation;
+                this.displayCalc = this._operation.join(" "); 
                 break;
 
         
@@ -80,38 +91,100 @@ class CalcController {
                 break;
         }
     }
+
+    // Define o valor de erro no display
     setError() {
         this.displayCalc = "Error";
     }
+
+    // Retorna o último valor da operação
     getLastOperation() {
         return this._operation[this._operation.length - 1];
     }
-    addOperation(value){
-        if (isNaN(this.getLastOperation())) {
-           
-            this._operation.push(value);
-        } else {
-            this._operation[this._operation.length - 1] = this.getLastOperation().toString() + value.toString();
 
-            
-        }
-
-
+    // Define o último valor da operação
+    // substituindo o último operador do array de operações
+    setLastOperation(value) {
+        this._operation[this._operation.length - 1] = value;
     }
+
+    // Verifica se o valor é um operador
+    isOperator(value) {
+        return (['+', '-', '*', '/', '%'].indexOf(value) > -1);
+    }
+
+    // Adiciona uma operação ao array de operações
+    // e executa o cálculo se o array de operações tiver 3 elementos
+    pushOperation(value) {
+       
+        this._operation.push(value);
+        
+        if (this._operation.length > 3) {            
+            this.calc();        
+        }
+    }
+
+    // Avalia a expressão matemática do array de operações
+    // e substitui o array de operações pelo resultado e o último operador
+    calc(){
+
+        let last = this._operation.pop();
+        let result = eval(this._operation.join(" ")); 
+        this._operation = [result, last];
+    }
+
+    // Adiciona uma operação ao array de operações  this._operation[this._operation.length - 1]
+    addOperation(value) {
+        console.log(value);
+        if (isNaN(this.getLastOperation())) { 
+            // Última operação é um operador
+            if (this.isOperator(value)) {
+                // Substitui o operador anterior
+                this.setLastOperation(value);
+            } else if (!isNaN(value)) {
+                // Adiciona um número após um operador
+                this.pushOperation(parseInt(value));
+            } 
+        } else {
+            // Última operação é um número
+            if (this.isOperator(value)) {
+                // Adiciona o operador como uma nova posição
+                this.pushOperation(value);
+            }else {
+                // Concatena o número ao último número
+                let newValue = parseInt(this.getLastOperation().toString() + value.toString());
+                this.setLastOperation(newValue);
+            }
+        }
+    }
+
+    // Limpa todas as operações
+    // e redefine o display para "0"
     clearAll() {
         this._operation = [];
         this.displayCalc = "0";
     }
+
+    // Limpa a última entrada da operação
+    // e redefine o display para o último valor da operação
     clearEntry() {
         this._operation.pop();
-        this.displayCalc = this._operation.join("");
+        if (this._operation.length == 0) {
+            this.displayCalc = "0";
+        } else {
+            this.displayCalc = this.getLastOperation();
+        }
     }
+
+    // Adiciona um ouvinte de eventos a um elemento
+    // e executa o manipulador de eventos para os eventos especificados
     addEventListenerAll(element, events, handler) {
         events.split(' ').forEach(event => {
             element.addEventListener(event, handler, false);
         });
     }
 
+    // Define o formato de data e hora para o display
     setDisplayDateTime() {
         this.displayTime = this.correntDate.toLocaleTimeString(this._locale, {
             hour: "2-digit",
